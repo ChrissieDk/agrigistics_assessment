7e7ad555da1933a2bd22e46cf711d70ea8ccd62c
ChrissieDk <christiaandk96@gmail.com>
2025-10-22 20:28:51 +0200

    feat: add left and right action sidebars

--- agrigistics-payroll/src/app/app.component.ts
+++ agrigistics-payroll/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { AsyncPipe, NgIf } from '@angular/common';
 import { PayrollStore } from './state/payroll.store';
 import { PayrollTableComponent } from './components/payroll-table/payroll-table.component';
 import { EmployeeInfoPanelComponent } from './components/employee-info-panel/employee-info-panel.component';
+import { RightSidebarComponent } from './components/right-sidebar/right-sidebar.component';
+import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, AsyncPipe, PayrollTableComponent, EmployeeInfoPanelComponent],
+  imports: [RouterOutlet, NgIf, AsyncPipe, PayrollTableComponent, EmployeeInfoPanelComponent, RightSidebarComponent, LeftSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
